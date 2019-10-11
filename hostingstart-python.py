19fa1811f4573ed92e575ec9877bd1a2e463ae7c
Silas Helgesson <39592299+SilasHelgesson@users.noreply.github.com>
2019-10-11 20:30:35 +0200

    Update hostingstart-python.py

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 import platform
 
 var = 3
@@ -7,10 +7,11 @@ var = 3
 def application(environ, start_response):
 
     start_response(b'200 OK', [(b'Content-Type', b'text/html')])
-    if(var == 3):
+    os.system('python app.py')
+    #if(var == 3):
         #with open ("hostingstart-python.html", "r") as hostingstart_file:
-        with open ("app.py", "r") as hostingstart_file:
-            hosting = hostingstart_file.read()
+        #with open ("app.py", "r") as hostingstart_file:
+            #hosting = hostingstart_file.read()
     
         
         yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
