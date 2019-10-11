575d4058f665c23ff31e6b3e241d73436bbb9acc
Silas Helgesson <39592299+SilasHelgesson@users.noreply.github.com>
2019-10-11 20:16:24 +0200

    Update hostingstart-python.py

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -9,9 +9,9 @@ def application(environ, start_response):
     start_response(b'200 OK', [(b'Content-Type', b'text/html')])
     
     with open ("hostingstart-python.html", "r") as hostingstart_file:
-
-        hosting = hostingstart_file.read()
-        
+    
+       # hosting = hostingstart_file.read()
+       hosting = "hello world"
 
         yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
 
